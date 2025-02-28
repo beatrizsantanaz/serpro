64fe59088878d23ff3fe969defe521779cc8eeaf
beatrizsantanaz <beatriz.davila03@gmail.com>
2025-02-28 15:33:04 -0300

    sei lá

--- src/app.js
+++ src/app.js
@@ -4,6 +4,10 @@ const guiaRoutes = require("./routes/guiaRoutes");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.send("✅ API do Serpro rodando corretamente! Use as rotas /api/das para acessar os serviços.");
+});
+
 app.use(express.json()); // Suporte para JSON no corpo da requisição
 app.use("/api", guiaRoutes); // Aqui registramos as rotas do DAS
 
