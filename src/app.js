c233a4c7ddcca7c5a54cabe56eaf5fa1774b2875
beatrizsantanaz <beatriz.davila03@gmail.com>
2025-03-17 16:03:02 -0300

    Codigo de Autenticação Procurador

--- src/app.js
+++ src/app.js
@@ -1,8 +1,17 @@
 const express = require("express");
 const guiaRoutes = require("./routes/guiaRoutes");
-
+const { obterTokenAutenticacao } = require('./config/serproAuth');
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.get('/get-token', async (req, res) => {
+  try {
+      const token = await obterTokenAutenticacao();
+      res.json({ token });
+  } catch (error) {
+      res.status(500).json({ erro: 'Erro ao obter token de autenticação' });
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("✅ API do Serpro rodando corretamente! Use as rotas /api/das para acessar os serviços.");
